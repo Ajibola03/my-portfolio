0ea0cdccc519886c25ee24b3817298e4d1c2881a
Ajibola03 <atoyebieniola93@gmail.com>
2021-08-19 08:13:31 +0100

    mail error fix

--- controllers/contact.php
+++ controllers/contact.php
@@ -9,6 +9,9 @@ $email = htmlspecialchars(trim($_POST['email']));
 $subject = htmlspecialchars(trim($_POST['subject']));
 $message = htmlspecialchars(trim($_POST['message']));
 
-$mailSent = Mailer::send($email, $message, $subject, $name);
+$mailSent = Mailer::send('atoyebieniola93@gmail.com', $message, $subject, $name);
 $_SESSION['mail_sent'] = $mailSent;
+if($mailSent){
+    $mailSent = Mailer::send($email, "Your mail has been received.<br><br>I will get back to you within 48 hours", "Reply from Atoyebi AJibola", "Atoyebi Ajibola");
+}
 header('location: ../');
